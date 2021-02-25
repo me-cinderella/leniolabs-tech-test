a474ff01c084731538e7398ce6636b30c3a601de
me-cinderella <mariana.encalada.ap@gmail.com>
2021-02-25 12:24:53 -0300

    added footer to app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header/CustomHeader';
+import Footer from './components/Footer/CustomFooter';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <main>
         <h1>App</h1>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ app/src/components/Footer/CustomFooter.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CustomFooter() {
+    return (
+        <footer className="bg-dark text-center text-white fixed-bottom">
+          <div className="text-center p-3">
+            © 2021 Copyright
+          </div>
+        </footer>
+    )
+}
+
+export default CustomFooter;
